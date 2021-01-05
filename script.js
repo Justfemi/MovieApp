8176b87fcf1651d6669b32c89229658192094797
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2021-01-05 14:27:23 +0100

    api consumed

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const form = document.getElementById('form');
 
 const search = document.getElementById('search');
 
+const main = document.getElementById('main');
+
 //Get initial movies
 getMovies(API_URL);
 
@@ -16,6 +18,43 @@ async function getMovies(url) {
     const data = await res.json();
 
     showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, 
+            overview } = movie ;   
+        
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate (vote) {
+    if (vote >= 8) {
+        return 'green';
+    }
+    else if (vote >= 5) {
+        return 'orange';
+    }
+    else {
+        return 'red';
+    }
 }
 
 //Search for movies
@@ -32,5 +71,4 @@ form.addEventListener('submit', (e) => {
     else {
         window.location.reload();
     }
-});
-
+}); 
